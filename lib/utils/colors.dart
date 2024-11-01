7bdd52589bf6c5354093d9cafca69ba5a5b142a8
sendhyrama <sendhyrama@gmail.com>
2024-11-01 15:01:10 +0700

    feat: add utils (colors and text-sytles)

---
+++ lib/utils/colors.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class PrimaryColor {
+  static const Color c1 = Color(0xFFE0F2FE);
+  static const Color c2 = Color(0xFFBAE6FD);
+  static const Color c3 = Color(0xFF7DD3FC);
+  static const Color c4 = Color(0xFF38BDF8);
+  static const Color c5 = Color(0xFF0EA5E9);
+}
+
+class NeutralColor {
+  static const Color c1 = Color(0xFFE8E5DE);
+  static const Color c2 = Color(0xFFC0BDB6);
+  static const Color c3 = Color(0xFFACA9A2);
+  static const Color c4 = Color(0xFF98958E);
+  static const Color c5 = Color(0xFF84817A);
+  static const Color c6 = Color(0xFF706D66);
+  static const Color c7 = Color(0xFF5C5952);
+  static const Color c8 = Color(0xFF48453E);
+  static const Color c9 = Color(0xFF201D16);
+}
+
+class SuccessColor {
+  static const Color c1 = Color(0xFFD9EFDA);
+  static const Color c2 = Color(0xFFB4DEB5);
+  static const Color c3 = Color(0xFF8ECE91);
+  static const Color c4 = Color(0xFF69BD6C);
+  static const Color c5 = Color(0xFF43AD47);
+  static const Color c6 = Color(0xFF368A39);
+  static const Color c7 = Color(0xFF28682B);
+  static const Color c8 = Color(0xFF1B451C);
+  static const Color c9 = Color(0xFF0D230E);
+}
+
+class DangerColor {
+  static const Color c1 = Color(0xFFF8D8DA);
+  static const Color c2 = Color(0xFFF2B2B4);
+  static const Color c3 = Color(0xFFEB8B8F);
+  static const Color c4 = Color(0xFFE56569);
+  static const Color c5 = Color(0xFFDE3E44);
+  static const Color c6 = Color(0xFFB23236);
+  static const Color c7 = Color(0xFF852529);
+  static const Color c8 = Color(0xFF59191B);
+  static const Color c9 = Color(0xFF2C0C0E);
+}
+
+class WarningColor {
+  static const Color c1 = Color(0xFFFFFADB);
+  static const Color c2 = Color(0xFFFFF5B7);
+  static const Color c3 = Color(0xFFFFF194);
+  static const Color c4 = Color(0xFFFFEC70);
+  static const Color c5 = Color(0xFFFFE74C);
+  static const Color c6 = Color(0xFFD3BF3D);
+  static const Color c7 = Color(0xFFA7972E);
+  static const Color c8 = Color(0xFF7B6E1E);
+  static const Color c9 = Color(0xFF4F460F);
+}
