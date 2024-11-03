241d8ca6edcec4bb54eb0ef202c3bf4c9d9adb23
sendhyrama <sendhyrama@gmail.com>
2024-11-03 13:54:02 +0700

    feat(widgets): add custom button

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:virtusee_slicing_ui/utils/colors.dart';
+import 'package:virtusee_slicing_ui/utils/text_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color color;
+  final Color textColor;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.color = PrimaryColor.c5,
+    this.textColor = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyles.b3
+              .copyWith(color: textColor, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
